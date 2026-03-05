88838ff655df162827de1d8e307d40ff39c06b1d
Pavel Kamburov <pkamburov89@gmail.com>
2026-03-05 22:45:28 +0200

    Create WeatherCard.tsx

---
+++ components/ui/WeatherCard.tsx
@@ -0,0 +1,72 @@
+import { CloudSun, Droplets, Sun, Wind } from "lucide-react";
+import { Card } from "./Card";
+import { WeatherData } from "@/features/weather/types";
+import { getWeatherLabel } from "@/features/weather/utiils";
+
+type WeatherDataProps = {
+  weatherData: WeatherData;
+};
+
+export function WeatherCard({ weatherData }: WeatherDataProps) {
+  const code = getWeatherLabel(weatherData.current.weatherCode);
+  return (
+    <div>
+      <Card className="p-6 bg-linear-to-br from-blue-50 to-cyan-50 border-blue-100">
+        <div className="flex items-start justify-between">
+          <div className="space-y-4">
+            <div className="flex items-center gap-3">
+              <CloudSun className="w-12 h-12 text-blue-600" />
+              <div>
+                <p className="text-sm text-gray-600">Current Weather</p>
+                <p className="text-3xl font-semibold text-gray-900">
+                  {weatherData.current.temperature}C
+                </p>
+                <p className="text-gray-700">{code}</p>
+              </div>
+            </div>
+
+            <div className="grid grid-cols-3 gap-6 pt-4">
+              <div className="flex items-center gap-2">
+                <Droplets className="w-5 h-5 text-blue-500" />
+                <div>
+                  <p className="text-xs text-gray-600">Precipitation Chance</p>
+                  <p className="text-lg font-semibold text-gray-900">
+                    {weatherData.daily[0].precipitation}%
+                  </p>
+                </div>
+              </div>
+              <div className="flex items-center gap-2">
+                <Sun className="w-5 h-5 text-amber-500" />
+                <div>
+                  <p className="text-xs text-gray-600">Max Temp</p>
+                  <p className="text-lg font-semibold text-gray-900">
+                    {weatherData.daily[0].tempMax}
+                  </p>
+                </div>
+              </div>
+              <div className="flex items-center gap-2">
+                <Wind className="w-5 h-5 text-gray-500" />
+                <div>
+                  <p className="text-xs text-gray-600">Wind speed</p>
+                  <p className="text-lg font-semibold text-gray-900">
+                    {weatherData.current.windSpeed} km/h
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="hidden md:block">
+            <div className="bg-white/50 rounded-lg p-4 space-y-2">
+              <p className="text-sm font-medium text-gray-700">Garden Tips</p>
+              <p className="text-sm text-gray-600">
+                Perfect conditions for watering. UV levels are moderate - good
+                for most indoor plants near windows.
+              </p>
+            </div>
+          </div>
+        </div>
+      </Card>
+    </div>
+  );
+}
