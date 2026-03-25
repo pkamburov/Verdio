7e00fcc181dc5e6a19d45519326bff9980cf1f59
Pavel Kamburov <pkamburov89@gmail.com>
2026-03-25 13:43:40 +0200

    Update utils.ts

--- features/weather/utils.ts
+++ features/weather/utils.ts
@@ -1,8 +1,14 @@
 export function getWeatherLabel(code: number) {
   if (code === 0) return "Clear sky";
-  if (code <= 3) return "Cloudy";
-  if (code >= 61 && code <= 67) return "Rain";
-  if (code >= 71 && code <= 77) return "Snow";
+  if (code <= 3) return "Partly cloudy";
+  if (code === 45 || code === 48) return "Fog";
+  if (code >= 51 && code <= 57) return "Drizzle";
+
+  if ((code >= 61 && code <= 67) || (code >= 80 && code <= 82)) return "Rain";
+
+  if ((code >= 71 && code <= 77) || (code >= 85 && code <= 86)) return "Snow";
+
+  if (code >= 95) return "Thunderstorm";
 
   return "Unknown";
 }
